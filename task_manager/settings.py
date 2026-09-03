88d97460de6d654b7d4d17136fb453a3a4ab8e3d
Spring-Silver-Bird <adelaida_19@mail.ru>
2026-09-03 16:55:50 +0300

    dinamic DB

--- task_manager/settings.py
+++ task_manager/settings.py
@@ -90,10 +90,8 @@ WSGI_APPLICATION = 'task_manager.wsgi.application'
 # https://docs.djangoproject.com/en/6.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    "default":
+        {"ENGINE": "django.db.backends.sqlite3", "NAME": BASE_DIR / "db.sqlite3"}
 }
 
 
