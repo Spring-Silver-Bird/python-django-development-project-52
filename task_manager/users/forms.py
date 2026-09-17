6f5fb45ee19b60606b2cf24711d2779b3bf28880
Spring-Silver-Bird <adelaida_19@mail.ru>
2026-09-17 22:32:38 +0300

    fix

--- task_manager/users/forms.py
+++ task_manager/users/forms.py
@@ -1,5 +1,7 @@
+from django import forms
 from django.contrib.auth import get_user_model
 from django.contrib.auth.forms import UserCreationForm
+from django.core.exceptions import ValidationError
 
 User = get_user_model()
 
@@ -19,7 +21,10 @@ class RegisterForm(UserCreationForm):
         }
 
 
-class UpdateForm(UserCreationForm):
+class UpdateForm(forms.ModelForm):
+    password1 = forms.CharField(label="Пароль", widget=forms.PasswordInput, required=False)
+    password2 = forms.CharField(label="Подтверждение пароля", widget=forms.PasswordInput, required=False)
+
     class Meta:
         model = User
         fields = ["first_name", "last_name", "username"]
@@ -30,3 +35,21 @@ class UpdateForm(UserCreationForm):
             "password1": "Пароль",
             "password2": "Подтверждение пароля",
         }
+
+    def clean(self):
+        cleaned = super().clean()
+        password1 = cleaned.get("password1")
+        password2 = cleaned.get("password2")
+        if password1 or password2:
+            if password1 != password2:
+                raise ValidationError("Введенные пароли не совпадают")
+        return cleaned
+
+    def save(self, commit=True):
+        user = super().save(commit=False)
+        password1 = self.cleaned_data.get("password1")
+        if password1:
+            user.set_password(password1)
+        if commit:
+            user.save()
+        return user
