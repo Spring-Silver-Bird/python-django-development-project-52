48a7c12944f3d86ad793b5cbb147cf03015bbc4e
Spring-Silver-Bird <adelaida_19@mail.ru>
2026-09-17 22:09:02 +0300

    executor optional

--- task_manager/tasks/migrations/0004_alter_task_executor.py
+++ task_manager/tasks/migrations/0004_alter_task_executor.py
@@ -6,16 +6,22 @@ from django.db import migrations, models
 
 
 class Migration(migrations.Migration):
-
     dependencies = [
-        ('tasks', '0003_rename_label_task_labels_alter_task_status'),
+        ("tasks", "0003_rename_label_task_labels_alter_task_status"),
         migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.AlterField(
-            model_name='task',
-            name='executor',
-            field=models.ForeignKey(blank=True, null=True, on_delete=django.db.models.deletion.PROTECT, related_name='executed_tasks', to=settings.AUTH_USER_MODEL, verbose_name='Исполнитель'),
+            model_name="task",
+            name="executor",
+            field=models.ForeignKey(
+                blank=True,
+                null=True,
+                on_delete=django.db.models.deletion.PROTECT,
+                related_name="executed_tasks",
+                to=settings.AUTH_USER_MODEL,
+                verbose_name="Исполнитель",
+            ),
         ),
     ]
